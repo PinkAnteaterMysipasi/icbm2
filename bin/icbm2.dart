ad4b546092b4ed852f6866b6e13ac07126f58383
PinkAnteaterMysipasi <rostislav.kozlik@seznam.cz>
2023-08-17 17:22:28 +0200

    updated gitignore

--- bin/icbm2.dart
+++ bin/icbm2.dart
@@ -5,5 +5,10 @@ import 'package:dotenv/dotenv.dart';
 void main(List<String> args) async {
   final env = DotEnv()..load();
   await pb.admins.authWithPassword(env['PB_USER']!, env['PB_PASS']!);
+  for (final exercise in await pb.collection('exercises').getFullList()) {
+    await pb.collection('exercises').update(exercise.id, body: {
+      'attempts': {},
+    });
+  }
   await webapp.listen(int.tryParse(args.firstOrNull ?? '3000') ?? 3000);
 }
